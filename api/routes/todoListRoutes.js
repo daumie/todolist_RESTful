40399ccae5b1ebd13175c52fa8fdb9e843669a64
Dominic Motuka <dominic.motuka@gmail.com>
2018-09-28 10:27:33 +0300

    Add applications routing

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,18 @@
+// Routing refers to determining how an application responds to a client request 
+// for a specific endpoint, which is a URI (or path) and a specific HTTP request 
+// method (GET, POST, and so on).
+
+module.exports = function (app) {
+  const todoList = require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
